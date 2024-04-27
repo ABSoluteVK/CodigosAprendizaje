69c392382d4e7fd026da65130915ec42474fa7fb
ABSoluteVK <168141408+ABSoluteVK@users.noreply.github.com>
2024-04-27 02:42:41 +0200

    Update CambioUnidades.py

--- CambioUnidades.py
+++ CambioUnidades.py
@@ -1,35 +1,103 @@
 #Se van a convertir algunas unidades como Volumen, Temperatura, Velocidades, Areas, Peso y Longitudes
-def longitud(unidad, cambio):
-    print('\n Elige la unidad a la que quieres ')
+def longitud(a):
 
-def peso():
-    print('peso')
+    unidad, cambio = PedirUnidades(a)
 
-def volumen():
-    print('volumen')
+    if unidad == 'kilometros' and cambio == 'millas':
+        valor = float(input('\nQue valor de Km quieres convertir: '))
+        calculo = valor*0.621371
 
-def temperatura():
-    print('temperatura')
+    elif unidad == 'kilometros' and cambio == 'pies':
+        valor = float(input('\nQue valor de Km quieres convertir: '))
+        calculo = valor*3280.84
 
-def area():
-    print('area')
+    elif unidad == 'kilometros' and cambio == 'pulgadas':
+        valor = float(input('\nQue valor de Km quieres convertir: '))
+        calculo = valor*39370.08
 
-def velocidad():
-    print('velocidad')
+    elif unidad == 'millas' and cambio == 'kilometros':
+        valor = float(input('\nQue valor de millas quieres convertir: '))
+        calculo = valor*1.60934
 
-def unidades(unidad, conversion, tipounidad):
+    elif unidad == 'millas' and cambio == 'pies':
+        valor = float(input('\nQue valor de millas quieres convertir: '))
+        calculo = valor*5280
+
+    elif unidad == 'millas' and cambio == 'pulgadas':
+        valor = float(input('\nQue valor de millas quieres convertir: '))
+        calculo = valor*63360
+
+    elif unidad == 'pies' and cambio == 'kilometros':
+        valor = float(input('\nQue valor de pies quieres convertir: '))
+        calculo = valor*0.0003048
+
+    elif unidad == 'pies' and cambio == 'millas':
+        valor = float(input('\nQue valor de pies quieres convertir: '))
+        calculo = valor*0.000189393939
+
+    elif unidad == 'pies' and cambio == 'pulgadas':
+        valor = float(input('\nQue valor de pies quieres convertir: '))
+        calculo = valor*12
+
+    elif unidad == 'pulgadas' and cambio == 'pulgadas':
+        valor = float(input('\nQue valor de pulgadas quieres convertir: '))
+        calculo = valor*0.0000254
+
+    elif unidad == 'pulgadas' and cambio == 'pulgadas':
+        valor = float(input('\nQue valor de pulgadas quieres convertir: '))
+        calculo = valor*0.0000157828283
+
+    elif unidad == 'pulgadas' and cambio == 'pulgadas':
+        valor = float(input('\nQue valor de pulgadas quieres convertir: '))
+        calculo = valor/12
+
+    return round(calculo, 5)
+
+def peso(a):
+    unidad, cambio = PedirUnidades(a)
+
+def volumen(a):
+    unidad, cambio = PedirUnidades(a)
+
+def temperatura(a):
+    unidad, cambio = PedirUnidades(a)
+
+def area(a):
+    unidad, cambio = PedirUnidades(a)
+
+def velocidad(a):
+    unidad, cambio = PedirUnidades(a)
+
+def unidades(tipounidad):
+    # Todas las unidades
     unidad = {'longitudes': ['millas','pies', 'kilometros', 'pulgadas'],
               'pesos': ['kilogramos', 'libras', 'onzas'],
               'volumen': ['litos', 'galones', 'onzas liquidas'],
               'temperatura': ['kelvin', 'celcius', 'fahrenheit'],
               'area': ['metros cuadrados', 'pies cuadrados','acres'],
               'velocidad': ['kilometro hora', 'pies hora', 'millas hora']}
-    print(f'Elige tus unidades, aqui te dejo todas tus opciones:')
+    #Bucle con para guardar todas las unidades en la lista
+    lista_unidades = []
+
     for i in range(len(unidad[tipounidad])):
-        print(f'{unidad[tipounidad][i].capitalize()}', end= ' ')
+        lista_unidades.append(unidad[tipounidad][i])
 
-    unidad = input('\n\nElige el tipo de unidad que quieres convertir: ')
+    return lista_unidades
 
+def PedirUnidades(lista):
+    #Bucle para comprobar y volver a pedir las unidades
+    while True:
+        print('\nTus opciones son las siguientes:')
+        for i in range(len(lista)):
+            print(f'{i+1}) {lista[i].capitalize()}  ')
+
+        unidad = input('\nElige tu unidad: ')
+        cambio = input('Elige la unidad a la que quieres convertirla: ')
+        if unidad.lower() in lista and cambio.lower() in lista:
+            break
+        else:
+            print('\nTu unidad o el cambio elegido no existe entre las opciones!')
+    return unidad, cambio
 #Bucle programa principal 
 while True:
     #imprime las opciones a elegir y pide la seleccion
@@ -40,33 +108,36 @@ while True:
     for i in range(len(lista)):
         print(f'{i + 1} - {lista[i].capitalize()}')
 
-    seleccion = input('Elige el tipo de unidades que quieras convertir: ')
+    seleccion = input('\nElige una opcion: ')
 
     # comprobamos las selecciones y llamamos a la funcion encargada de hacer los calculos depues de elegir las unidades
     if seleccion.lower() == lista[-1]:
         break
 
     elif seleccion.lower() == lista[0]:
-        print('\n Elige las unidades que quieres convertir\n'.upper().center(149, '-'))
-        unidad = ''
-        cambio = ''
-        unidades(unidad, cambio, 'longitudes')
-        longitud(unidad, cambio)
+        unidades_cambio = unidades('longitudes')
+        solucion = longitud(unidades_cambio)
+        print(f'\nEl nuevo valor de tus unidades es: {solucion}\n')
 
     elif seleccion.lower() == lista[1]:
-        peso()
+        unidades_cambio = unidades('pesos')
+        peso(unidades_cambio)
 
     elif seleccion.lower() == lista[2]:
-        volumen()
+        unidades_cambio = unidades('volumenes')
+        volumen(unidades_cambio)
 
     elif seleccion.lower() == lista[3]:
-        temperatura()
+        unidades_cambio = unidades('temperaturas')
+        temperatura(unidades_cambio)
 
     elif seleccion.lower() == lista[4]:
-        area()
+        unidades_cambio = unidades('areas')
+        area(unidades_cambio)
 
     elif seleccion.lower() == lista[5]:
-        velocidad()
+        unidades_cambio = unidades('velocidades')
+        velocidad(unidades_cambio)
 
     else:
         print('\nPor favor, introduce una respuesta valida\n')
