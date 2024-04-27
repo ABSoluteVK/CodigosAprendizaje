2b5d8611f29c56b280f81ca65ed28c499e893ccd
ABSoluteVK <168141408+ABSoluteVK@users.noreply.github.com>
2024-04-27 15:21:32 +0200

    Update CambioUnidades.py

--- CambioUnidades.py
+++ CambioUnidades.py
@@ -1,72 +1,105 @@
 #Se van a convertir algunas unidades como Volumen, Temperatura, Velocidades, Areas, Peso y Longitudes
-def longitud(a):
+def conversion(unidad, cambio, valor):
+    conversiones = {
+        'longitudes': {
+            'kilometros': {
+                'millas': 0.621371,
+                'pies': 3280.84,
+                'pulgadas': 39370.08
+            },
+            'millas': {
+                'kilometros': 1.60934,
+                'pies': 5280,
+                'pulgadas': 63360
+            },
+            'pies': {
+                'kilometros': 0.0003048,
+                'millas': 0.000189393939,
+                'pulgadas': 12
+            },
+            'pulgadas': {
+                'kilometros': 0.0000254,
+                'millas': 0.0000157828283,
+                'pies': 1/12
+            }
+        },
+        'pesos': {
+            'kilogramos': {
+                'libras': 2.20462,
+                'onzas': 35.274
+            },
+            'libras': {
+                'kilogramos': 0.453592,
+                'onzas': 16
+            },
+            'onzas': {
+                'kilogramos': 0.0283495,
+                'libras': 0.0625
+            }
+        },
+        'volumen': {
+            'litros': {
+                'galones': 0.264172,
+                'onzas liquidas': 33.814
+            },
+            'galones': {
+                'litros': 3.78541,
+                'onzas liquidas': 128
+            },
+            'onzas liquidas': {
+                'litros': 0.0295735,
+                'galones': 0.0078125
+            }
+        },
+        'temperatura': {
+            'kelvin': {
+                'celcius': lambda x: x - 273.15,
+                'fahrenheit': lambda x: x * 9/5 - 459.67
+            },
+            'celcius': {
+                'kelvin': lambda x: x + 273.15,
+                'fahrenheit': lambda x: x * 9/5 + 32
+            },
+            'fahrenheit': {
+                'kelvin': lambda x: (x + 459.67) * 5/9,
+                'celcius': lambda x: (x - 32) * 5/9
+            }
+        },
+        'area': {
+            'metros cuadrados': {
+                'pies cuadrados': 10.7639,
+                'acres': 0.000247105
+            },
+            'pies cuadrados': {
+                'metros cuadrados': 0.092903,
+                'acres': 0.0000229568
+            },
+            'acres': {
+                'metros cuadrados': 4046.86,
+                'pies cuadrados': 43560
+            }
+        },
+        'velocidad': {
+            'kilometro hora': {
+                'pies hora': 3280.84,
+                'millas hora': 0.621371
+            },
+            'pies hora': {
+                'kilometro hora': 0.0003048,
+                'millas hora': 0.000189393939
+            },
+            'millas hora': {
+                'kilometro hora': 1.60934,
+                'pies hora': 5280
+            }
+        }
+    }
 
-    unidad, cambio = PedirUnidades(a)
-
-    if unidad == 'kilometros' and cambio == 'millas':
-        valor = float(input('\nQue valor de Km quieres convertir: '))
-        calculo = valor*0.621371
-
-    elif unidad == 'kilometros' and cambio == 'pies':
-        valor = float(input('\nQue valor de Km quieres convertir: '))
-        calculo = valor*3280.84
-
-    elif unidad == 'kilometros' and cambio == 'pulgadas':
-        valor = float(input('\nQue valor de Km quieres convertir: '))
-        calculo = valor*39370.08
-
-    elif unidad == 'millas' and cambio == 'kilometros':
-        valor = float(input('\nQue valor de millas quieres convertir: '))
-        calculo = valor*1.60934
-
-    elif unidad == 'millas' and cambio == 'pies':
-        valor = float(input('\nQue valor de millas quieres convertir: '))
-        calculo = valor*5280
-
-    elif unidad == 'millas' and cambio == 'pulgadas':
-        valor = float(input('\nQue valor de millas quieres convertir: '))
-        calculo = valor*63360
-
-    elif unidad == 'pies' and cambio == 'kilometros':
-        valor = float(input('\nQue valor de pies quieres convertir: '))
-        calculo = valor*0.0003048
-
-    elif unidad == 'pies' and cambio == 'millas':
-        valor = float(input('\nQue valor de pies quieres convertir: '))
-        calculo = valor*0.000189393939
-
-    elif unidad == 'pies' and cambio == 'pulgadas':
-        valor = float(input('\nQue valor de pies quieres convertir: '))
-        calculo = valor*12
-
-    elif unidad == 'pulgadas' and cambio == 'pulgadas':
-        valor = float(input('\nQue valor de pulgadas quieres convertir: '))
-        calculo = valor*0.0000254
-
-    elif unidad == 'pulgadas' and cambio == 'pulgadas':
-        valor = float(input('\nQue valor de pulgadas quieres convertir: '))
-        calculo = valor*0.0000157828283
-
-    elif unidad == 'pulgadas' and cambio == 'pulgadas':
-        valor = float(input('\nQue valor de pulgadas quieres convertir: '))
-        calculo = valor/12
-
+    if callable(conversiones[unidad][cambio]):
+        calculo = conversiones[unidad][cambio]
+    else:
+        calculo = valor * conversiones[unidad][cambio]
     return round(calculo, 5)
-
-def peso(a):
-    unidad, cambio = PedirUnidades(a)
-
-def volumen(a):
-    unidad, cambio = PedirUnidades(a)
-
-def temperatura(a):
-    unidad, cambio = PedirUnidades(a)
-
-def area(a):
-    unidad, cambio = PedirUnidades(a)
-
-def velocidad(a):
-    unidad, cambio = PedirUnidades(a)
 
 def unidades(tipounidad):
     # Todas las unidades
@@ -99,45 +132,26 @@ def PedirUnidades(lista):
             print('\nTu unidad o el cambio elegido no existe entre las opciones!')
     return unidad, cambio
 #Bucle programa principal 
+
 while True:
-    #imprime las opciones a elegir y pide la seleccion
     print('CONVERTIDOR DE UNIDADES'.center(31,'-'))
 
-    lista = ['longitud','peso','volumen','temperatura','area','velocidad','salir']
+    lista = ['longitudes','pesos','volumenes','temperaturas','areas','velocidades','salir']
 
     for i in range(len(lista)):
         print(f'{i + 1} - {lista[i].capitalize()}')
 
     seleccion = input('\nElige una opcion: ')
 
-    # comprobamos las selecciones y llamamos a la funcion encargada de hacer los calculos depues de elegir las unidades
     if seleccion.lower() == lista[-1]:
         break
 
-    elif seleccion.lower() == lista[0]:
-        unidades_cambio = unidades('longitudes')
-        solucion = longitud(unidades_cambio)
-        print(f'\nEl nuevo valor de tus unidades es: {solucion}\n')
-
-    elif seleccion.lower() == lista[1]:
-        unidades_cambio = unidades('pesos')
-        peso(unidades_cambio)
-
-    elif seleccion.lower() == lista[2]:
-        unidades_cambio = unidades('volumenes')
-        volumen(unidades_cambio)
-
-    elif seleccion.lower() == lista[3]:
-        unidades_cambio = unidades('temperaturas')
-        temperatura(unidades_cambio)
-
-    elif seleccion.lower() == lista[4]:
-        unidades_cambio = unidades('areas')
-        area(unidades_cambio)
-
-    elif seleccion.lower() == lista[5]:
-        unidades_cambio = unidades('velocidades')
-        velocidad(unidades_cambio)
+    elif seleccion.lower() in lista[:-1]:  # Si la selección está en la lista de unidades
+        unidades_cambio = unidades(seleccion.lower())  # Obtenemos las unidades de cambio
+        unidad, cambio = PedirUnidades(unidades_cambio)  # Pedimos al usuario que elija las unidades
+        valor = float(input(f'\nQue valor de {unidad} quieres convertir: '))  # Pedimos al usuario que ingrese el valor a convertir
+        solucion = conversion(unidad, cambio, valor)  # Realizamos la conversión
+        print(f'\nEl nuevo valor de tus unidades es: {solucion}\n')  # Mostramos el resultado
 
     else:
         print('\nPor favor, introduce una respuesta valida\n')
