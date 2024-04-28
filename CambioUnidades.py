cdcd2fcf2a050c0c36fcf094a26afee7f8541c65
ABSoluteVK <168141408+ABSoluteVK@users.noreply.github.com>
2024-04-28 02:26:42 +0200

    .

--- CambioUnidades.py
+++ CambioUnidades.py
@@ -1,97 +1,98 @@
 from tkinter import *
+from tkinter import ttk
 #Se van a convertir algunas unidades como Volumen, Temperatura, Velocidades, Areas, Peso y Longitudes
 def conversion(seleccion, unidad, cambio, valor):
     conversiones = {
-        'longitudes': {
-            'kilometros': {
-                'millas': 0.621371,
-                'pies': 3280.84,
-                'pulgadas': 39370.08
+        'Longitudes': {
+            'Kilometros': {
+                'Millas': 0.621371,
+                'Pies': 3280.84,
+                'Pulgadas': 39370.08
             },
-            'millas': {
-                'kilometros': 1.60934,
-                'pies': 5280,
-                'pulgadas': 63360
+            'Millas': {
+                'Kilometros': 1.60934,
+                'Pies': 5280,
+                'Pulgadas': 63360
             },
-            'pies': {
-                'kilometros': 0.0003048,
-                'millas': 0.000189393939,
-                'pulgadas': 12
+            'Pies': {
+                'Kilometros': 0.0003048,
+                'Millas': 0.000189393939,
+                'Pulgadas': 12
             },
-            'pulgadas': {
-                'kilometros': 0.0000254,
-                'millas': 0.0000157828283,
-                'pies': 1/12
+            'Pulgadas': {
+                'Kilometros': 0.0000254,
+                'Millas': 0.0000157828283,
+                'Pies': 1/12
             }
         },
-        'pesos': {
-            'kilogramos': {
-                'libras': 2.20462,
-                'onzas': 35.274
+        'Pesos': {
+            'Kilogramos': {
+                'Libras': 2.20462,
+                'Onzas': 35.274
             },
-            'libras': {
-                'kilogramos': 0.453592,
-                'onzas': 16
+            'Libras': {
+                'Kilogramos': 0.453592,
+                'Onzas': 16
             },
-            'onzas': {
-                'kilogramos': 0.0283495,
-                'libras': 0.0625
+            'Onzas': {
+                'Kilogramos': 0.0283495,
+                'Libras': 0.0625
             }
         },
-        'volumenes': {
-            'litros': {
-                'galones': 0.264172,
-                'onzas liquidas': 33.814
+        'Volumenes': {
+            'Litros': {
+                'Galones': 0.264172,
+                'Onzas liquidas': 33.814
             },
-            'galones': {
-                'litros': 3.78541,
-                'onzas liquidas': 128
+            'Galones': {
+                'Litros': 3.78541,
+                'Onzas liquidas': 128
             },
-            'onzas liquidas': {
-                'litros': 0.0295735,
-                'galones': 0.0078125
+            'Onzas liquidas': {
+                'Litros': 0.0295735,
+                'Galones': 0.0078125
             }
         },
-        'temperaturas': {
-            'kelvin': {
-                'celcius': lambda x: round(x - 273.15),
-                'fahrenheit': lambda x: round(x * 9/5 - 459.67)
+        'Temperaturas': {
+            'Kelvin': {
+                'Celcius': lambda x: round(x - 273.15),
+                'Fahrenheit': lambda x: round(x * 9/5 - 459.67)
             },
-            'celcius': {
-                'kelvin': lambda x: round(x + 273.15),
-                'fahrenheit': lambda x: round(x * 9/5 + 32)
+            'Celcius': {
+                'Kelvin': lambda x: round(x + 273.15),
+                'Fahrenheit': lambda x: round(x * 9/5 + 32)
             },
-            'fahrenheit': {
-                'kelvin': lambda x: round((x + 459.67) * 5/9),
-                'celcius': lambda x: round((x - 32) * 5/9)
+            'Fahrenheit': {
+                'Kelvin': lambda x: round((x + 459.67) * 5/9),
+                'Celcius': lambda x: round((x - 32) * 5/9)
             }
         },
-        'areas': {
-            'metros cuadrados': {
-                'pies cuadrados': 10.7639,
-                'acres': 0.000247105
+        'Areas': {
+            'Metros cuadrados': {
+                'Pies cuadrados': 10.7639,
+                'Acres': 0.000247105
             },
-            'pies cuadrados': {
-                'metros cuadrados': 0.092903,
-                'acres': 0.0000229568
+            'Pies cuadrados': {
+                'Metros cuadrados': 0.092903,
+                'Acres': 0.0000229568
             },
-            'acres': {
-                'metros cuadrados': 4046.86,
-                'pies cuadrados': 43560
+            'Acres': {
+                'Metros cuadrados': 4046.86,
+                'Pies cuadrados': 43560
             }
         },
-        'velocidades': {
-            'kilometro hora': {
-                'pies hora': 3280.84,
-                'millas hora': 0.621371
+        'Velocidades': {
+            'Kilometro hora': {
+                'Pies hora': 3280.84,
+                'Millas hora': 0.621371
             },
-            'pies hora': {
-                'kilometro hora': 0.0003048,
-                'millas hora': 0.000189393939
+            'Pies hora': {
+                'Kilometro hora': 0.0003048,
+                'Millas hora': 0.000189393939
             },
-            'millas hora': {
-                'kilometro hora': 1.60934,
-                'pies hora': 5280
+            'Millas hora': {
+                'Kilometro hora': 1.60934,
+                'Pies hora': 5280
             }
         }
     }
@@ -104,43 +105,29 @@ def conversion(seleccion, unidad, cambio, valor):
         calculo = round(calculo, 5)
         return calculo
 
-def unidades(tipounidad):
-    # Todas las unidades
-    unidad = {'longitudes': ['millas','pies', 'kilometros', 'pulgadas'],
-              'pesos': ['kilogramos', 'libras', 'onzas'],
-              'volumenes': ['litros', 'galones', 'onzas liquidas'],
-              'temperaturas': ['kelvin', 'celcius', 'fahrenheit'],
-              'areas': ['metros cuadrados', 'pies cuadrados','acres'],
-              'velocidades': ['kilometro hora', 'pies hora', 'millas hora']}
-    #Bucle con para guardar todas las unidades en la lista
-    lista_unidades = []
+def on_select(event):
+    # Esta función se ejecutará cuando el usuario seleccione una opción
+    seleccion = BDesplegable.get()
+    print("El usuario seleccionó:", seleccion)
+    if seleccion in unidad:
+        opciones = Label(frame, text=f"\n".join(unidad[seleccion]), bg= 'silver', pady=10)
+        opciones.grid(row=2, column=0, sticky='we')
+    else:
+        print('WTF')
 
-    for i in range(len(unidad[tipounidad])):
-        lista_unidades.append(unidad[tipounidad][i])
+lista = ['Longitudes','Pesos','Volumenes','Temperaturas','Areas','Velocidades']
 
-    return lista_unidades
-
-def PedirUnidades(lista):
-    #Bucle para comprobar y volver a pedir las unidades
-    while True:
-        print('\nTus opciones son las siguientes:')
-        for i in range(len(lista)):
-            print(f'{i+1}) {lista[i].capitalize()}  ')
-
-        unidad = input('\nElige tu unidad: ')
-        cambio = input('Elige la unidad a la que quieres convertirla: ')
-        if unidad.lower() in lista and cambio.lower() in lista:
-            break
-        else:
-            print('\nTu unidad o el cambio elegido no existe entre las opciones!')
-    return unidad, cambio
-#Bucle programa principal 
-
+unidad = {'Longitudes': ['Millas','Pies', 'Kilometros', 'Pulgadas'],
+              'Pesos': ['Kilogramos', 'Libras', 'Onzas'],
+              'Volumenes': ['Litros', 'Galones', 'Onzas Liquidas'],
+              'Temperaturas': ['Kelvin', 'Celcius', 'Fahrenheit'],
+              'Areas': ['Metros cuadrados', 'Pies cuadrados','Acres'],
+              'Velocidades': ['Kilometro hora', 'Pies hora', 'Millas hora']}
 """
 while True:
     print('CONVERTIDOR DE UNIDADES'.center(31,'-'))
 
-    lista = ['longitudes','pesos','volumenes','temperaturas','areas','velocidades','salir']
+    
 
     for i in range(len(lista)):
         print(f'{i + 1} - {lista[i].capitalize()}')
@@ -180,6 +167,14 @@ ventana.resizable(0, 0)
 ventana.config(background='silver')
 
 frame = Frame(ventana)
-BDesplegable = Radiobutton(frame, text="Unidades a elegir: ")
+frame.pack()
+descripcion = Label(frame, text='En este programa podras elegir entre varios tipos de unidades para ver que conversiones tienes disponibles para realizar.', background='silver', wraplength=550)
+descripcion.grid(row=0, column=0)
+
+BDesplegable = ttk.Combobox(frame, text="Unidades a elegir", values=lista, font= ('Arial', 20), background='silver')
+# Vincula el evento '<<ComboboxSelected>>' a la función on_select
+BDesplegable.bind("<<ComboboxSelected>>", on_select)
+BDesplegable.grid(row=1, column= 0)
+
 
 ventana.mainloop()
