a4ae7b593dc737dca3b975289bc956a5804aba6f
ABSoluteVK <168141408+ABSoluteVK@users.noreply.github.com>
2024-07-11 18:47:22 +0200

    Update AdivinaElNumero.py

--- AdivinaElNumero.py
+++ AdivinaElNumero.py
@@ -1,69 +1,62 @@
-from random import *
-#Funcion para jugar al juego
+from random import randint
+
 def Juego(intentos, rango_min, rango_max):
-    a = 1
     numeroSecreto = randint(rango_min, rango_max)
-    while a < intentos+1:
-        print(f'Intento numero {a}'.center(40,'-'))
-        acierto = int(input('Dame tu numero: '))
+    for intento in range(1, intentos + 1):
+        print(f'Intento número {intento}'.center(40, '-'))
+        while True:
+            try:
+                acierto = int(input('Dame tu número: '))
+                break
+            except ValueError:
+                print("Por favor, introduce un número válido.")
         if numeroSecreto == acierto:
-            break
+            print(f'¡Felicidades! Has ganado en {intento} intentos.')
+            return
+        elif acierto > numeroSecreto:
+            print('El número que buscas es más pequeño.')
         else:
-            if acierto > numeroSecreto:
-                print('El numero que buscas es mas pequeño.')
-            else:
-                print('El numero que buscas es mas grande.')
-            a += 1
-    
-    if a == intentos and numeroSecreto == acierto:
-        print("Felecidades has ganado por los pelos!")
-    elif a == intentos and numeroSecreto != acierto:
-        print(f'El numero corresto era {numeroSecreto}. Has perdido suerte la proxima vez!')
-    elif numeroSecreto == acierto and a < intentos:
-        print(f'Felicidades has ganado en {a} aciertos!')
-    else:
-        print("wtf")
+            print('El número que buscas es más grande.')
+    print(f'El número correcto era {numeroSecreto}. ¡Has perdido, suerte la próxima vez!')
 
-
-#Lista para almacenar los niveles de dificultad
-modos = ['facil', 'medio', 'dificil','imposible']
-#Almacenamos las reglas en un diccionario
+modos = ['fácil', 'medio', 'difícil', 'imposible']
 reglas = {
-    'facil':'Tendras que adivinar un numero entre el 1 y el 100 en tan solo 10 intentos.',
-    'medio':'Tendras que adivinar un numero entre el 1 y el 1000 en tan solo 10 intentos.',
-    'dificil':'Tendras que adivinar un numero entre el 1 y el 10000 en tan solo 10 intentos.',
-    'imposible':'Tendras que adivinar un numero entre el 1 y el 100 en tan solo 3 intentos.'
-    }
+    'fácil': 'Tendrás que adivinar un número entre el 1 y el 100 en tan solo 10 intentos.',
+    'medio': 'Tendrás que adivinar un número entre el 1 y el 1000 en tan solo 10 intentos.',
+    'difícil': 'Tendrás que adivinar un número entre el 1 y el 10000 en tan solo 10 intentos.',
+    'imposible': 'Tendrás que adivinar un número entre el 1 y el 100 en tan solo 3 intentos.'
+}
 
-print(f'Selecciona el modo de juego'.center(51, '-'))
-
-for posicion in range(len(modos)):
-    print(f'{posicion + 1}) {modos[posicion].capitalize()}')
-
-#Creamos un bucle para preguntar que modo de juego quiere jugar
+print('Selecciona el modo de juego'.center(51, '-'))
+for posicion, modo in enumerate(modos, start=1):
+    print(f'{posicion}) {modo.capitalize()}')
+print('')
 while True:
-    Dificultad = int(input('Selecciona el modo de juego (1-4): '))
-    #Comprobamos que el modo de juego seleccionado es valido
-    if Dificultad > 0 and Dificultad <= len(modos):
-        #Mostramos las normas antes de continuar con el juego y le preguntamos si esta seguro que quiere jugar en este modo
-        print('Las reglas del modo de juego son las siguientes'.center(51, '-'))
-        print(reglas[modos[Dificultad - 1]])
-        ContinuarJuego = int(input('Confirmar juego Si(1)/No(2): '))
-        if ContinuarJuego == 1:
-            break
-        elif ContinuarJuego == 2:
-            continue
+    try:
+        Dificultad = int(input('Selecciona el modo de juego (1-4): '))
+        if 1 <= Dificultad <= len(modos):
+            print(' ')
+            print('Las reglas del modo de juego son las siguientes'.center(51, '-'))
+            print(reglas[modos[Dificultad - 1]])
+            print('')
+            ContinuarJuego = int(input('Confirmar juego Sí(1)/No(2): '))
+            print('')
+            if ContinuarJuego == 1:
+                break
+            elif ContinuarJuego == 2:
+                continue
+            else:
+                print('Error!')
         else:
-            print('Error!')
-    else:
-        print('Tienes que elegir una dificultad valida! (Un numero que corresponda a una dificultad)')
-if Dificultad-1 == 0:
-    Juego(10,1,100)
-elif Dificultad-1 == 1:
-    Juego(10,1,1000)
-elif Dificultad-1 == 2:
-    Juego(10,1,10000)
-elif Dificultad-1 == 3:
-    Juego(3,1,100)
-else:
-    print('WTF')
+            print('¡Tienes que elegir una dificultad válida! (Un número que corresponda a una dificultad)')
+    except ValueError:
+        print("Por favor, introduce un número válido.")
+
+if Dificultad == 1:
+    Juego(10, 1, 100)
+elif Dificultad == 2:
+    Juego(10, 1, 1000)
+elif Dificultad == 3:
+    Juego(10, 1, 10000)
+elif Dificultad == 4:
+    Juego(3, 1, 100)
